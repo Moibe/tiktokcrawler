8fbce4c774c97824c3390323bed6fc8e0d6549f5
Moibe <moi_estrello@hotmail.com>
2023-11-23 15:42:12 -0600

    .gitignore

--- init.py
+++ init.py
@@ -1,29 +1,28 @@
 import time
 from webdrivers import createDriver
-#from workspaces import Google
+from workspaces import Google
 from manyvids import ManyVids
-
 
 #Aquí llamas a otras funciones en otros py que realizen el trabajo.
 #instagram(obtenerUsuarios)
 #tiktok(obtenerusuarios)
 
 #Google(servicio='busqueda', busqueda='Mexico')
-#Google(servicio='busqueda', busqueda='Finlandia')
+Google(servicio='busqueda', busqueda='Finlandia')
 #ManyVids(servicio='signIn')
 
 #Crea un navegador con las características que deseas.
-navegador = createDriver(navigator='chrome', width=800, height=600, headless=False)
+#navegador = createDriver(navigator='chrome', width=800, height=600, headless=False)
 
 #Creo que para muchas funciones no se necesita hacer signIn!
 #print("Haciendo SignIn")
 #ManyVids(navegador, servicio = 'signIn')
 
-time.sleep(1)
+#time.sleep(1)
 
 #Para ésto sirve hacerlo modularmente, podemos saltarnos procesos que ya hicimos. 
-print("Listando creadores...")
-ManyVids(navegador, servicio = 'listCreators')
+#print("Listando creadores...")
+#ManyVids(navegador, servicio = 'listCreators')
 
 #Y partir hacia otros caminos después del signIn.
 
